75579d49c343fbf049be99c06d6ab9eb222f2c46
Kononiuk Anton <hmasteraaa@gmail.com>
2023-09-03 15:51:45 +0300

    bot features added

--- src/Controllers/BotController.js
+++ src/Controllers/BotController.js
@@ -1,5 +1,6 @@
 const { saveProduct, updateProductStatus } = require('../Storages/ProductStorage');
 const { saveUser } = require('../Storages/UserStorage');
+const { commands } = require('../utils/commands');
 
 module.exports = (app, bot) => {
   const Status = {
@@ -9,13 +10,20 @@ module.exports = (app, bot) => {
   };
   const adminId = process.env.ADMIN_ID;
   // Matches "/echo [whatever]"
-  bot.onText(/\/start/, async (msg, match) => {
+  bot.setMyCommands(commands);
+  bot.onText(/\/start/, async (msg) => {
     try {
       await bot.sendMessage(msg.chat.id, `Вы запустили бота!`);
 
       if (msg.text === '/start') {
-        await bot.sendMessage(msg.chat.id, `Вітаю! `);
-        await bot.sendMessage(msg.chat.id, `${match} match `);
+        await bot.sendMessage(
+          msg.chat.id,
+          `Вітаю! Трохи варіанті верстки тексту: HTML приклад \n\n<b>Потрібен текст для вітання сюди </b>\n<i>TODO</i>\n<code>Можна скопіювати</code>\n<s>Варіант тексту</s>\n<u>Ще варіант</u>\n<pre language='c++'>Додатковий код</pre>\n<a href='t.me'>Посилання якщо потрібно</a>`,
+          {
+            parse_mode: 'HTML',
+          },
+        );
+        // await bot.sendMessage(msg.chat.id, `${match} match `);
       }
     } catch (error) {
       console.log(error);
@@ -30,7 +38,7 @@ module.exports = (app, bot) => {
     console.log(msg.from.id, msg.from.username);
     // send a message to the chat acknowledging receipt of their message
     await bot.sendMessage(chatId, 'хай');
-    await bot.sendMessage(adminId, `Юзер ${msg.from.username} написав тобі месагу: ${msg.text}`);
+    await bot.sendMessage(adminId, `Юзер ${msg.from.username} написав тобі адміну месагу: ${msg.text}`);
     if (msg.text === 'save product') {
       const result = await saveProduct({
         clientId: from.id,
@@ -80,6 +88,36 @@ module.exports = (app, bot) => {
         await bot.sendMessage(adminId, `Помилка зміни статусу у БД!`);
       }
     }
+
+    if (msg.text == '/menu') {
+      await bot.sendMessage(msg.chat.id, `Меню бота`, {
+        reply_markup: {
+          keyboard: [
+            ['⭐️ Тарифы', '⭐️ Правила'],
+            ['⭐️ Написать нам', '⭐️ Канал'],
+          ],
+          resize_keyboard: true,
+        },
+      });
+    }
+
+    bot.on('text', async (msg) => {
+      try {
+        if (msg.text == '/help') {
+          await bot.sendMessage(
+            msg.chat.id,
+            `Розділ допомоги HTML\n\n<b>Жирный Текст</b>\n<i>Текст Курсивом</i>\n<code>Текст с Копированием</code>\n<s>Перечеркнутый текст</s>\n<u>Подчеркнутый текст</u>\n<pre language='c++'>код на c++</pre>\n<a href='t.me'>Гиперссылка</a>`,
+            {
+              parse_mode: 'HTML',
+            },
+          );
+        } else {
+          await bot.sendMessage(msg.chat.id, msg.text);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
   });
 };
 
---
+++ src/utils/commands.js
@@ -0,0 +1,16 @@
+const commands = [
+  {
+    command: 'start',
+    description: 'Запуск бота',
+  },
+  {
+    command: 'menu',
+    description: 'Продивитись меню',
+  },
+  {
+    command: 'help',
+    description: 'Розділ допомоги',
+  },
+];
+
+module.exports = { commands };
