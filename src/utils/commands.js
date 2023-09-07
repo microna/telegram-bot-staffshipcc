150760d5c314a7337dc01d73d77c135036455532
Kononiuk Anton <hmasteraaa@gmail.com>
2023-09-07 09:08:19 +0300

    Dev-4 update bot file structure

--- src/utils/commands.js
+++ src/utils/commands.js
@@ -1,15 +1,11 @@
 const commands = [
   {
-    command: 'start',
-    description: 'Запуск бота',
-  },
-  {
     command: 'menu',
-    description: 'Продивитись меню',
+    description: 'Просмотретьь меню',
   },
   {
     command: 'help',
-    description: 'Розділ допомоги',
+    description: 'Получить помощь',
   },
 ];
 
