c897423fc3f84e707a475f0088fe5979203bd178
Kononiuk Anton <hmasteraaa@gmail.com>
2023-09-16 14:39:40 +0300

    Dev-6 add auth and add linter in frontend

--- frontend/src/auth-state/route-wrappers/private.tsx
+++ frontend/src/auth-state/route-wrappers/private.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthState } from '../use-auth-state.hook';
 
 export const Private = () => {
-   const { user } = useAuthState();
-   console.log(user);
-   if (!user) {
-      return <Navigate to='sign-in' replace />;
-   }
+  const { user } = useAuthState();
+  if (!user) {
+    return <Navigate to="sign-in" replace />;
+  }
 
-   return <Outlet />;
+  return <Outlet />;
 };
--- frontend/src/pages/mainpage/mainpage.tsx
+++ frontend/src/pages/mainpage/mainpage.tsx
@@ -1,50 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'auth-state/use-auth-state.hook';
+import { axiosPrivate } from 'api/axios';
 
 export interface IProduct {
-   productText: string;
-   status: string;
-   userTGId: number;
-   comments: string;
-   createdAt: string;
-   updatedAt: string;
+  productText: string;
+  status: string;
+  userTGId: number;
+  comments: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
+const getProducts = async () => {
+  try {
+    const result = await axiosPrivate.get('products');
+    return result.data;
+  } catch (e) {
+    console.log('error get products');
+  }
+};
 const Mainpage = () => {
-   const [products, setProducts] = useState<[] | null>(null);
-   const [error, setError] = useState<string>('');
-   const backendUrl = process.env.REACT_APP_BACKEND_URL || '/';
-   const [state, setState] = useState(false);
+  const [products, setProducts] = useState<[] | null>(null);
+  const { clearToken } = useAuthState();
 
-   useEffect(() => {
-      try {
-         axios.get(backendUrl + 'productsasdasd').then((result) => {
-            setProducts(result.data);
-         });
-      } catch (err) {
-         setError('error');
-      }
-   // eslint-disable-next-line react-hooks/exhaustive-deps
-   }, []);
+  useEffect(() => {
+    handleGetProducts();
+  }, []);
 
-   const handleProduct = () => {
-      setState(!state);
-   };
-   console.log('dasd', error);
-   return (
-      <div>
-         {state &&
-            products?.map((product: IProduct, index) => {
-               return (
-                  <div key={index}>
-                     {' '}
-                     <div>{product.productText}</div>{' '}
-                     <button onClick={handleProduct}>fsdfsdfsd</button>
-                  </div>
-               );
-            })}
-      </div>
-   );
+  const handleGetProducts = async () => {
+    const result = await getProducts();
+    setProducts(result);
+  };
+  return (
+    <div>
+      {products?.map((product: IProduct, index) => {
+        return (
+          <div key={index}>
+            {' '}
+            <div>{product.productText}</div>{' '}
+          </div>
+        );
+      })}
+      <button onClick={handleGetProducts}>Get products</button>
+      <button className="absolute right-10 top-5" onClick={() => clearToken()}>
+        Loguot
+      </button>
+    </div>
+  );
 };
 
 export default Mainpage;
