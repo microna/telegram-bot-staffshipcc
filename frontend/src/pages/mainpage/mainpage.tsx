7667fc178c4952b0e7f54f29fdaa80e223671eed
Kononiuk Anton <hmasteraaa@gmail.com>
2023-09-16 11:31:29 +0300

    Dev-6 add frontend

--- frontend/src/pages/mainpage/mainpage.tsx
+++ frontend/src/pages/mainpage/mainpage.tsx
@@ -24,6 +24,7 @@ const Mainpage = () => {
       } catch (err) {
          setError('error');
       }
+   // eslint-disable-next-line react-hooks/exhaustive-deps
    }, []);
 
    const handleProduct = () => {
