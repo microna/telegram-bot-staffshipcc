471ee5aae473a9df0ff4a85c681e24d74896668a
microna <kievonfire@gmail.com>
2023-11-21 15:43:17 +0200

    Dev-9-add-tostify

--- frontend/src/pages/mainpage/component/tablePage.tsx
+++ frontend/src/pages/mainpage/component/tablePage.tsx
@@ -7,6 +7,7 @@ import { HiPencilAlt } from 'react-icons/hi';
 import { IProduct, Status } from '../mainpage';
 import { axiosPrivate } from 'api/axios';
 import classNames from 'classnames';
+import { toast } from 'react-toastify';
 
 interface TableProductsPageProps {
   product: IProduct;
@@ -40,7 +41,9 @@ const EditProductModal: FC<TableProductsPageProps> = function ({
   const [status, setStatus] = useState(product.status);
   const [isOpenDropDown, setIsOpenDropDown] = useState(false);
   const [productDetails, setProductDetails] = useState('');
+
   const [loading, setLoading] = useState(false);
+
   const handleChangeProductStatus = async () => {
     setLoading(true);
     const result = await changeProductStatus({
@@ -72,6 +75,13 @@ const EditProductModal: FC<TableProductsPageProps> = function ({
       }
     }
   };
+
+  // const copyToClipboard = (text: string) => {
+  //   navigator.clipboard
+  //     .writeText(text)
+  //     .then(() => setCopySuccess('Copied to clipboard'))
+  //     .catch((error) => console.error('Unable to copy to clipboard', error));
+  // };
   return (
     <>
       {loading && (
@@ -95,8 +105,10 @@ const EditProductModal: FC<TableProductsPageProps> = function ({
       >
         <Modal.Header className="bg-gray-60 border-b border-gray-200 !p-6 dark:border-gray-700">
           <strong>
-            <span className="mx-5 text-blue-900"> Edit product.</span>{' '}
-            <span className="mx-5 text-blue-900">User:</span>{' '}
+            <span className="mx-5 text-blue-900 dark:text-gray-100">
+              Edit product.
+            </span>{' '}
+            <span className="mx-5 text-blue-900 dark:text-gray-100">User:</span>{' '}
             <span className="text-white">
               {' '}
               <a
@@ -107,7 +119,9 @@ const EditProductModal: FC<TableProductsPageProps> = function ({
                 {product.userTGNick}
               </a>
             </span>
-            <span className="ml-5 text-blue-900">Status: </span>
+            <span className="ml-5 text-blue-900 dark:text-gray-100">
+              Status:{' '}
+            </span>
             <span className="text-blue-400">{product.status}</span>
           </strong>
         </Modal.Header>
@@ -225,6 +239,12 @@ export const ProductsTable: FC<TableProductsPageProps> = function ({
   product,
   handleGetProducts,
 }) {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const trackNumber = product.trackNumber;
+    navigator.clipboard.writeText(trackNumber);
+    toast.success(`Copied track number: ${trackNumber}`);
+  };
   return (
     <Table.Row
       key={product._id}
@@ -238,14 +258,15 @@ export const ProductsTable: FC<TableProductsPageProps> = function ({
       <Table.Cell className="truncate overflow-hidden max-w-[200px] whitespace-nowrap p-4 text-base font-medium text-gray-900 dark:text-black ">
         {product.updatedAt}
       </Table.Cell>
-      <Table.Cell className="truncate overflow-hidden max-w-[200px] font-medium whitespace-nowrap p-4">
-        {product.info}
-      </Table.Cell>
+
       <Table.Cell className="truncate overflow-hidden max-w-[200px] whitespace-nowrap p-4 text-base font-medium text-gray-900 dark:text-black w-[120px]">
         {product.status}
       </Table.Cell>
       <Table.Cell className="truncate overflow-hidden max-w-[200px] whitespace-nowrap p-4 text-base font-medium text-gray-900 dark:text-black ">
-        {product.trackNumber}
+        <a className="cursor-pointer" href="#" onClick={handleClick}>
+          {' '}
+          {product.trackNumber}
+        </a>
       </Table.Cell>
       <Table.Cell className="truncate overflow-hidden max-w-[200px] whitespace-nowrap p-4 text-base font-medium text-gray-900 dark:text-black ">
         <a
